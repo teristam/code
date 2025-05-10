902108a60e7c528e65f9411afa2f52879274f2e8
Teris Tam <teristam@gmail.com>
2025-05-10 16:12:18 +0100

    use pyqtgraph to show the image

--- test/socket/example_client.py
+++ test/socket/example_client.py
@@ -4,6 +4,10 @@ import numpy as np
 import json
 import base64
 import msgpack
+from PyQt6 import QtWidgets
+import pyqtgraph as pg
+import sys
+
 class ImageClient:
     def __init__(self, sub_address="tcp://localhost:5555", push_address="tcp://localhost:5556"):
         self.context = zmq.Context()
@@ -25,11 +29,11 @@ class ImageClient:
             topic, metadata_packed, image_bytes = self.sub_socket.recv_multipart()
             metadata = msgpack.unpackb(metadata_packed)
             print(metadata)
+            print(topic)
 
-            if topic.decode("utf-8") != "image":  # Filter by topic
+            if topic == b'image':  # Filter by topic
                 # Convert bytes to image array
                 img_array = np.frombuffer(image_bytes, dtype=np.uint8).reshape((metadata["height"], metadata["width"]))
-            
                 return img_array
             else:
                 return None
@@ -47,15 +51,24 @@ class ImageClient:
 
 
 if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    win = pg.ImageView()
+    win.show()
     image_socket = ImageClient()
-
-    while True:
+    timer = pg.QtCore.QTimer()
+    def update():
         img_array = image_socket.receive_image()
+        # print(img_array)
         if img_array is not None:
-            cv2.imshow("Client View", img_array)
+            win.setImage(img_array.T, autoLevels=True)  # Transpose if needed for correct orientation
 
         # Generate random coordinates for the box
         height, width = 100, 100
         top_left, bottom_right = image_socket.generate_random_box(width, height)
-        print("Sending message")
+        # print("Sending message")
         image_socket.send_draw_box_message(top_left, bottom_right)
+
+    timer.timeout.connect(update)
+    timer.start(100)  # update every 100 ms
+
+    sys.exit(app.exec())
